be377a00cba10842b593f0ec46c82811a14c6da5
Hadrien Grasland <grasland@lal.in2p3.fr>
2019-04-05 09:31:05 +0100

    Extract implementations to dedicated module

--- src/lib.rs
+++ src/lib.rs
@@ -1,163 +1,6 @@
+mod impls;
 pub(crate) mod thread_id;
 pub(crate) mod traits;
-
-use {
-    crate::{
-        thread_id::ThreadID,
-        traits::{Histogram, SyncHistogram},
-    },
-    std::{
-        cell::UnsafeCell,
-        ops::DerefMut,
-        sync::{
-            atomic::{AtomicUsize, Ordering},
-            Mutex,
-        },
-    },
-};
-
-// Simplest histogram that we can do performance tests with
-// One dimensional, everything has same weight, bin absciss in [0, 1[ range
-struct ToyHistogram {
-    bins: Vec<usize>,
-}
-
-impl ToyHistogram {
-    fn new(num_bins: usize) -> Self {
-        Self {
-            bins: vec![0; num_bins],
-        }
-    }
-}
-
-impl Histogram for ToyHistogram {
-    fn fill_mut(&mut self, values: &[f32]) {
-        for value in values {
-            let bin = f32::floor(value * (self.bins.len() as f32)) as usize;
-            self.bins[bin] += 1;
-        }
-    }
-
-    fn num_hits(&self) -> usize {
-        self.bins.iter().sum::<usize>()
-    }
-}
-
-// Basic thread-safe implementation that simply serializes using a lock
-impl SyncHistogram for Mutex<ToyHistogram> {
-    fn fill(&self, values: &[f32]) {
-        self.lock().unwrap().fill_mut(values)
-    }
-
-    fn num_hits(&self) -> usize {
-        self.lock().unwrap().num_hits()
-    }
-}
-
-// Slightly more advanced implementation which spreads accesses across a
-// configurable number of buckets to get overhead of TLS down.
-struct ThreadBucketizedHistogram {
-    buckets: Vec<Mutex<ToyHistogram>>,
-}
-
-impl ThreadBucketizedHistogram {
-    fn new(num_bins: usize, num_buckets: usize) -> Self {
-        Self {
-            buckets: (0..num_buckets).map(|_| Mutex::new(ToyHistogram::new(num_bins))).collect(),
-        }
-    }
-
-    fn lock_bucket(&self, id: ThreadID) -> impl DerefMut<Target=ToyHistogram> + '_ {
-        self.buckets[usize::from(id) % self.buckets.len()].lock().unwrap()
-    }
-}
-
-impl SyncHistogram for ThreadBucketizedHistogram {
-    fn fill(&self, values: &[f32]) {
-        self.fill_with_id(values, ThreadID::load())
-    }
-
-    fn fill_with_id(&self, values: &[f32], id: ThreadID) {
-        self.lock_bucket(id).fill_mut(values)
-    }
-
-    fn num_hits(&self) -> usize {
-        self.buckets.iter()
-            .map(|b| b.lock().unwrap().num_hits())
-            .sum::<usize>()
-    }
-}
-
-// More extreme cousin of ThreadBucketizedHistogram which assumes one bucket
-// per thread and uses that for lock elision
-struct ThreadLocalHistogram {
-    buckets: Vec<UnsafeCell<ToyHistogram>>,
-}
-
-impl ThreadLocalHistogram {
-    fn new(num_bins: usize) -> Self {
-        Self {
-            buckets: (0..num_cpus::get()).map(|_| UnsafeCell::new(ToyHistogram::new(num_bins))).collect(),
-        }
-    }
-
-    fn get_bucket(&self, id: ThreadID) -> impl DerefMut<Target=ToyHistogram> + '_ {
-        let bucket_ptr = self.buckets[usize::from(id) % self.buckets.len()].get();
-        unsafe { &mut *bucket_ptr }
-    }
-}
-
-impl SyncHistogram for ThreadLocalHistogram {
-    fn fill(&self, values: &[f32]) {
-        self.fill_with_id(values, ThreadID::load())
-    }
-
-    fn fill_with_id(&self, values: &[f32], id: ThreadID) {
-        self.get_bucket(id).fill_mut(values)
-    }
-
-    // BUG: This is actually NOT safe to call if other threads are filling concurrently
-    //
-    // To achieve this, we'd need to make the binds atomic and manipulate these using
-    // atomic loads and stores.
-    //
-    // Note that atomic read-modify-write operations are NOT needed in this case.
-    // 
-    fn num_hits(&self) -> usize {
-        self.buckets.iter()
-            .map(|b| unsafe { (*b.get()).num_hits() })
-            .sum::<usize>()
-    }
-}
-
-unsafe impl Send for ThreadLocalHistogram {}
-unsafe impl Sync for ThreadLocalHistogram {}
-
-// Completely different approach where buckets are modified using atomic RMW ops
-struct AtomicHistogram {
-    bins: Vec<AtomicUsize>,
-}
-
-impl AtomicHistogram {
-    fn new(num_bins: usize) -> Self {
-        Self {
-            bins: (0..num_bins).map(|_| AtomicUsize::new(0)).collect(),
-        }
-    }
-}
-
-impl SyncHistogram for AtomicHistogram {
-    fn fill(&self, values: &[f32]) {
-        for value in values {
-            let bin = f32::floor(value * (self.bins.len() as f32)) as usize;
-            self.bins[bin].fetch_add(1, Ordering::Relaxed);
-        }
-    }
-
-    fn num_hits(&self) -> usize {
-        self.bins.iter().map(|b| b.load(Ordering::Relaxed)).sum::<usize>()
-    }
-}
 
 
 #[cfg(test)]
@@ -168,7 +11,11 @@ mod tests {
         sync::Mutex,
         time::Instant,
     };
-    use super::*;
+    use crate::{
+        impls::*,
+        thread_id::*,
+        traits::*,
+    };
 
     // Parameters of the benchmarks are configured here
     const NUM_BINS: usize = 100;
